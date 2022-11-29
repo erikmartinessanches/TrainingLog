87cbd48e8fca7d99ce2c0722f5c3bd31529e7937
Erik Martines Sanches <erik.martinessanches@gmail.com>
2022-11-29 22:37:21 +0100

    Initialising firebase (FB).

--- src/persistence/Persistence.js
+++ src/persistence/Persistence.js
@@ -1,22 +1,15 @@
 import React from "react";
-import { firebaseConfig } from "../../config/firebaseConfig";
+import { firebaseConfig } from "../firebaseConfig";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
-//Import firebase
-function Persistence() {
-  /* Initialize Firebase. A Firebase App is a container-like object that stores 
+
+/* Initialize Firebase. A Firebase App is a container-like object that stores 
   common configuration and shares authentication across Firebase services. After 
   you initialize a Firebase App object in your code, you can add and start using 
   Firebase services. */
-  const app = initializeApp(firebaseConfig);
-
-  const analytics = getAnalytics(app);
-
-  // Initialize Realtime Database and get a reference to the service
-  const database = getDatabase(app);
-
-  //return <div>Persistence</div>;
-}
-
-export default Persistence;
+//I export what I need here.
+export const app = initializeApp(firebaseConfig);
+//export const analytics = getAnalytics(app);
+// Initialize Realtime Database and get a reference to the service
+export const database = getDatabase(app);
--- src/presenters/App.js
+++ src/presenters/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { app, database } from "../persistence/Persistence";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Mini project</header>
+      <div></div>
     </div>
   );
 }
