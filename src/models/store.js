0416ef188674929a66d022516ca9140ec4bce258
Erik Martines Sanches <erik.martinessanches@gmail.com>
2022-12-04 02:39:23 +0100

    configureStore() now makes use of both styles of middleware by including thunkMiddleware. This resolves the previous issue of only being able to use one type of middleware.

--- src/models/store.js
+++ src/models/store.js
@@ -2,19 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { fbMiddleware } from "./middleware";
 import { reducer } from "./reducers";
 import thunkMiddleware from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { createStore, applyMiddleware } from "redux";
+//import { composeWithDevTools } from "redux-devtools-extension";
+//import { createStore, applyMiddleware } from "redux";
 import { SaveNewRecord } from "./ThunkFunctions";
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+//const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
-// const store = configureStore({
-//   reducer: reducer,
-//   middleware: [SaveNewRecord, fbMiddleware],
-//   preloadedState: { user: null, records: [] },
-//   //enhancers: [composedEnhancer],
-// });
+const store = configureStore({
+  reducer: reducer,
+  middleware: [fbMiddleware, thunkMiddleware],
+  preloadedState: { user: null, records: [] },
+  //enhancers: [composedEnhancer],
+});
 
-const store = createStore(reducer, composedEnhancer);
+//const store = createStore(reducer, composedEnhancer);
 
 export default store;
