f5d342753ea9b6fbb978d7eda14e8c22aa58cc84
Erik Martines Sanches <erik.martinessanches@gmail.com>
2022-12-10 03:52:39 +0100

    Storing records under the same id in both firebase and redux store.

--- src/models/ThunkFunctions.jsx
+++ src/models/ThunkFunctions.jsx
@@ -1,23 +1,28 @@
-import { set, ref } from "firebase/database";
+import { set, ref, push } from "firebase/database";
 import { database } from "../persistence/Persistence";
-import { uuidv4 } from "@firebase/util";
 
 export function SaveNewRecord(text) {
   //We want to return the async thunk function.
 
   return async function SaveNewRecordThunk(dispatch, getState) {
     const userId = getState().user.data.uid;
-    /**TODO See this link about how to get a new record key from firebase.
-     * https://github.com/firebase/quickstart-js/blob/master/database/scripts/main.js
-    */
-    const newRecordId = uuidv4();
-    const REF = `users/${userId}/records/${newRecordId}`;
-    const initialRecord = { text: text };
-
+    const REF = `users/${userId}/records`;
+    const newRecord = { text: text };
+    /**We're obtaining the new id or 'key' for the next inserted item from
+     * firebase. Here we essentially say: Push() to the reference provided, but
+     * if we don't provide a value, as seen here, nothing will be written to the
+     * db and the child remains empty. We can use the reference to the child to,
+     * for example, get the new key. */
+    const newRecordId = push(ref(database, REF)).key;
+    debugger;
+    //push(ref(database, REF), initialRecord);
     /* Using set() overwrites data at the specified location, including any child 
     nodes. See if this data organization makes sense. */
-    await set(ref(database, REF), initialRecord );
+    await set(ref(database, `${REF}/${newRecordId}`), newRecord);
 
-    dispatch({ type: "RECORD_CREATED", payload: { ...initialRecord, recordId: newRecordId} });
+    dispatch({
+      type: "RECORD_CREATED",
+      payload: { ...newRecord, recordId: newRecordId },
+    });
   };
 }
