67cfe2f5143fa4d9f25735e4d629e98b8027ee9f
Erik Martines Sanches <erik.martinessanches@gmail.com>
2023-05-18 20:31:09 +0200

    Further improving the sign-up and login logic in AuthPresenter.

--- src/components/Auth/AuthPresenter.jsx
+++ src/components/Auth/AuthPresenter.jsx
@@ -1,27 +1,47 @@
-import React, { useState } from 'react'
-import AuthView from './AuthView'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import React, { useState, useEffect } from "react";
+import AuthView from "./AuthView";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useLocation } from "react-router-dom";
 
 function successACB() {
-  console.log("Signed in.")
+  console.log("Signed in.");
 }
 function failureACB(e) {
-  console.log(e.customData.message)
+  console.log(e.customData.message);
 }
 export default function AuthPresenter(props) {
-  const [signup, setSignup] = useState(true)
-  const [email, setEmail] = useState("") 
-  const [password, setPassword] = useState("")
+  const [signup, setSignup] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const location = useLocation();
+
+  useEffect(() => {
+    location.pathname === "/login" ? setSignup(false) : setSignup(true);
+  }, [location]);
 
   function signUpACB() {
-    createUserWithEmailAndPassword(props.auth, email, password).then(successACB).then(failureACB)
+    createUserWithEmailAndPassword(props.auth, email, password)
+      .then(successACB)
+      .then(failureACB);
   }
 
   function signInACB() {
-    signInWithEmailAndPassword(props.auth, email, password).then(successACB).catch(failureACB) 
+    signInWithEmailAndPassword(props.auth, email, password)
+      .then(successACB)
+      .catch(failureACB);
   }
 
   return (
-    <AuthView isSignup={signup} signUp={signUpACB} signIn={signInACB} setEmail={setEmail} setPassword={setPassword} toggleType={setSignup}/>
-  )
+    <AuthView
+      isSignup={signup}
+      signUp={signUpACB}
+      signIn={signInACB}
+      setEmail={setEmail}
+      setPassword={setPassword}
+      toggleType={setSignup}
+    />
+  );
 }
