2efdde558ff6d38cb41ae50a504958df89f2d4ac
Erik Martines Sanches <erik.martinessanches@gmail.com>
2023-11-22 14:13:58 +0100

    Removing old comments.

--- src/components/Auth/AuthPresenter.jsx
+++ src/components/Auth/AuthPresenter.jsx
@@ -24,21 +24,11 @@ export default function AuthPresenter(props) {
   const location = useLocation();
   const dispatch = useDispatch();
 
-  //const auth = getAuth(firebaseApp);
-  //const [authenticateNow, mutationResult] = useAuthenticateWithFirebaseMutation({fixedCacheKey: "logindata"});
-
-  /*   useEffect(() => {
-    location.pathname === "/login" ? setSignup(false) : setSignup(true);
-  }, [] ); */
   useEffect(() => {
     location.pathname === "/login" ? setSignup(false) : setSignup(true);
   }, [location.pathname]);
 
   async function onSubmitACB() {
-    //debugger;
-    //const yo = await authenticateNow({ email, password, usingAsSignUp: signup });
-    //const you = services;
-    //debugger;
     dispatch(
       registerOrLogIn({
         email,
@@ -48,33 +38,7 @@ export default function AuthPresenter(props) {
         lastName,
       })
     );
-    // createUserWithEmailAndPassword(props.auth, email, password)
-    //   .then(successACB)
-    //   .then(failureACB);
   }
-
-  /*   function signInACB() {
-    signInWithEmailAndPassword(props.auth, email, password)
-      .then(successACB)
-      .catch(failureACB);
-  } */
-  //debugger
-  // useEffect(() => mutationResult.requestId && console.log(mutationResult.data,"OUTSIDE CALLBACK ( isLoading and other data working ):",JSON.parse(JSON.stringify(mutationResult)))
-  // ,[mutationResult])
-
-  /**In this presenter I experiment with RTK Query for authenticating. RTK Query
-   * provides convenient hooks. I use useEffects below to set redux state. If I
-   * had done this with thunks, I would only dispatch the thunk once and it would
-   * eventually call the appropriate reducers in extra reducers in the state slice.
-   */
-  // useEffect(() => {
-  //   if (mutationResult.isSuccess &&  mutationResult.status==="fulfilled"){
-  //     console.log(mutationResult);
-  //     dispatch(setAuthFulfilled(mutationResult.data));
-  //   }
-  // }
-  // ,[dispatch, mutationResult])
-  //TODO: rejected and loading states.
 
   return (
     <AuthView
--- src/components/Dashboard/DashboardPresenter.jsx
+++ src/components/Dashboard/DashboardPresenter.jsx
@@ -10,42 +10,18 @@ import {
 } from "../../models/userSlice";
 import { LoadingIconView } from "../../views/LoadingIcon";
 import AuthPresenter from "../Auth/AuthPresenter";
-//import { updateModelFromFirebase } from "../persistence/firebaseModel";
 
 function DashboardPresenter() {
-  // const {
-  //   //We could use the current data in redux if we had a need for it in the view.
-  //   data: results,
-  //   error,
-  //   loading,
-  // } = useSelector((state) => state.user || {});
   const modelReady = useSelector(selectModelReady);
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   //const { logOut, loggedIn, loading, error } = useSecurity();
-  //const [shouldLogout, setShouldLogout] = useState(false);
-
-  // useEffect(() => {
-  //   if(shouldLogout)  {
-  //     navigate("/");
-  //     //dispatch(logoutNow());
-  //   }
-  // }, [shouldLogout])
 
   function logOutACB() {
-    //  console.log("Log the user out.");
-    //setShouldLogout(true);
     dispatch(logoutNow());
     //navigate("/");
-
-    //  logOut();
   }
-
-  // useEffect(() => {
-  //   updateModelFromFirebase();
-  //   //rerender?
-  // }, []);
 
   function createNewACB() {
     navigate("/dashboard/create-exercise");
