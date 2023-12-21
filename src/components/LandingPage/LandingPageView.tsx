c9c70b6649534579a299a40e454637be3fced761
Erik Martines Sanches <erik.martinessanches@gmail.com>
2023-12-21 20:10:38 +0100

    Continuing TS configuration.

--- src/components/LandingPage/LandingPageView.tsx
+++ src/components/LandingPage/LandingPageView.tsx
@@ -12,14 +12,14 @@ function LandingPageView() {
     navigate("/signup");
   }
 
-  console.log(`Hello from ${import.meta.env.REACT_APP_HELLO} environment!`)
+  console.log(`Hello from ${import.meta.env.VITE_APP_HELLO} environment!`)
 
   function toLogInACB() {
     navigate("/login");
   }
   return (
     <div className="App">
-      <header className="App-header">Mini project</header>
+      <header className="App-header">Training Log</header>
       <h2>App view (front page)</h2>
       <Button variant="contained" onClick={toSignUpACB}>
         Sign-up
